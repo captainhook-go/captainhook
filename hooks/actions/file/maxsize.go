26add43bd5e19d94524d2e69245548fa56ad9295
Sebastian Feldmann <sf@sebastian-feldmann.info>
2023-11-10 16:21:15 +0100

    Implement max size action

--- hooks/actions/file/maxsize.go
+++ hooks/actions/file/maxsize.go
@@ -1,10 +1,17 @@
 package file
 
 import (
+	"errors"
+	"fmt"
 	"github.com/captainhook-go/captainhook/configuration"
 	"github.com/captainhook-go/captainhook/git"
 	"github.com/captainhook-go/captainhook/hooks"
 	"github.com/captainhook-go/captainhook/io"
+	"math"
+	"os"
+	"regexp"
+	"strconv"
+	"strings"
 )
 
 type MaxSize struct {
@@ -16,10 +23,52 @@ func (a *MaxSize) IsApplicableFor(hook string) bool {
 }
 
 func (a *MaxSize) Run(action *configuration.Action) error {
-	// read configured file
-	// check if file is empty
 	a.hookBundle.AppIO.Write("checking max file size", true, io.VERBOSE)
+
+	size := action.Options().StringValueOf("max-size", "0")
+	sizeInBytes := a.toBytes(size)
+	if sizeInBytes == 0 {
+		return errors.New("the 'size' option is missing or wrong")
+	}
+	files, err := a.hookBundle.Repo.StagedFiles()
+	if err != nil {
+		return err
+	}
+	for _, path := range files {
+		file, err := os.Open(path)
+		if err != nil {
+			return err
+		}
+		stats, err := file.Stat()
+		if err != nil {
+			return fmt.Errorf("failed to read file at: %s", path)
+		}
+
+		if sizeInBytes < stats.Size() {
+			_ = file.Close()
+			return fmt.Errorf("file '%s' is bigger than the limit of %s", path, size)
+		}
+		_ = file.Close()
+	}
 	return nil
+}
+
+func (a *MaxSize) toBytes(value string) int64 {
+	matched, err := regexp.MatchString("^[0-9]*[BKMGTP]$", value)
+	if err != nil {
+		return 0
+	}
+	if !matched {
+		return 0
+	}
+	units := map[string]float64{"B": 0, "K": 1, "M": 2, "G": 3, "T": 4, "P": 5}
+	unit := strings.ToUpper(io.SubString(value, -1, 1))
+	number, err := strconv.ParseInt(io.SubString(value, 0, -1), 10, 64)
+	if err != nil {
+		return 0
+	}
+	unitBytes := math.Pow(1024, units[unit])
+	return number * int64(math.Round(unitBytes))
 }
 
 func NewMaxSize(appIO io.IO, conf *configuration.Configuration, repo *git.Repository) hooks.Action {
