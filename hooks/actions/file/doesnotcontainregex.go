2b0aca9506eca89d2dcfd3a65836300ce778437b
Sebastian Feldmann <sf@sebastian-feldmann.info>
2023-11-10 16:20:14 +0100

    Implement does not contain regex action

--- hooks/actions/file/doesnotcontainregex.go
+++ hooks/actions/file/doesnotcontainregex.go
@@ -1,10 +1,14 @@
 package file
 
 import (
+	"errors"
+	"fmt"
 	"github.com/captainhook-go/captainhook/configuration"
 	"github.com/captainhook-go/captainhook/git"
 	"github.com/captainhook-go/captainhook/hooks"
+	"github.com/captainhook-go/captainhook/info"
 	"github.com/captainhook-go/captainhook/io"
+	"regexp"
 )
 
 type DoesNotContainRegex struct {
@@ -16,15 +20,32 @@ func (a *DoesNotContainRegex) IsApplicableFor(hook string) bool {
 }
 
 func (a *DoesNotContainRegex) Run(action *configuration.Action) error {
-	// read configured file
-	// check regex
 	a.hookBundle.AppIO.Write("checking if file does not contain regex", true, io.VERBOSE)
+
+	reg := action.Options().StringValueOf("regex", "")
+	if reg == "" {
+		return errors.New("the 'regex' option is missing or empty")
+	}
+	files, err := a.hookBundle.Repo.StagedFiles()
+	if err != nil {
+		return err
+	}
+	for _, file := range files {
+		content, _ := io.ReadFile(file)
+		matched, readErr := regexp.MatchString(reg, string(content))
+		if readErr != nil {
+			return readErr
+		}
+		if matched {
+			return fmt.Errorf("the file '%s' contains the regex '%s'", file, reg)
+		}
+	}
 	return nil
 }
 
 func NewDoesNotContainRegex(appIO io.IO, conf *configuration.Configuration, repo *git.Repository) hooks.Action {
 	a := DoesNotContainRegex{
-		hookBundle: hooks.NewHookBundle(appIO, conf, repo, []string{}),
+		hookBundle: hooks.NewHookBundle(appIO, conf, repo, []string{info.PreCommit}),
 	}
 	return &a
 }
