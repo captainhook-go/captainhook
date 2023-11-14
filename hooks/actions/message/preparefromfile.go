f20597b2b756bb025d73eb55b7e09e7d3383d90f
Sebastian Feldmann <sebastianfeldmann@users.noreply.github.com>
2023-11-14 12:09:22 +0100

    Add basic docs

--- hooks/actions/message/preparefromfile.go
+++ hooks/actions/message/preparefromfile.go
@@ -8,6 +8,19 @@ import (
 	"github.com/captainhook-go/captainhook/io"
 )
 
+// PrepareFromFile loads a suggested commit message from a given file.
+// It is the counterpart of CacheOnFail. If your commit failed because of an
+// invalid message you can use the action to preload the failed message,
+// so you can edit it without having to completely write it from scratch again.
+//
+// Example configuration:
+//
+//	{
+//	  "run": "CaptainHook::Message.PrepareFromFile",
+//	  "options": {
+//	    "file": ".git/CH_COMMIT_MSG"
+//	  }
+//	}
 type PrepareFromFile struct {
 	hookBundle *hooks.HookBundle
 }
@@ -18,6 +31,7 @@ func (a *PrepareFromFile) IsApplicableFor(hook string) bool {
 
 func (a *PrepareFromFile) Run(action *configuration.Action) error {
 	a.hookBundle.AppIO.Write("prepare from file", true, io.VERBOSE)
+	// TODO: implement this
 	return nil
 }
 
