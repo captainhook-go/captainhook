b6217e2b88de76b2b071a96d4d90ad3c37dc5191
Sebastian Feldmann <sebastianfeldmann@users.noreply.github.com>
2023-11-16 14:58:57 +0100

    Colorize debug output

--- hooks/actions/debug/success.go
+++ hooks/actions/debug/success.go
@@ -30,7 +30,11 @@ func (a *Success) Run(action *configuration.Action) error {
 	a.hookBundle.AppIO.Write("debug action", true, io.VERBOSE)
 	a.hookBundle.AppIO.Write("<info>Hook Arguments</info>", true, io.NORMAL)
 	for name, value := range a.hookBundle.AppIO.Arguments() {
-		a.hookBundle.AppIO.Write(fmt.Sprintf("Name: %s Value: %s", name, value), true, io.NORMAL)
+		a.hookBundle.AppIO.Write(
+			fmt.Sprintf("  name: <comment>%s</comment> value: <comment>%s</comment>", name, value),
+			true,
+			io.NORMAL,
+		)
 	}
 	return nil
 }
