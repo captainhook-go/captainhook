1af6be2828e70458b53d2de43eb5d10b171bf6d8
Sebastian Feldmann <sebastianfeldmann@users.noreply.github.com>
2023-12-04 13:05:38 +0100

    Add docs

--- hooks/action.go
+++ hooks/action.go
@@ -3,6 +3,13 @@ package hooks
 import "github.com/captainhook-go/captainhook/configuration"
 
 type Action interface {
+	// IsApplicableFor answers if an `Action` can be used for a given hook
+	// For example actions that verify the commit message are only applicable for
+	// the `commit-message` hook.
 	IsApplicableFor(hook string) bool
+
+	// Run executes the action
+	// This will either execute some functionality provided from CaptainHook
+	// or some external executable.
 	Run(action *configuration.Action) error
 }
