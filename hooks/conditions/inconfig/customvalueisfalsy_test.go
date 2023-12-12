a689ba43b6518b8f4b87b65f5ee78cea3259b07a
Sebastian Feldmann <sebastianfeldmann@users.noreply.github.com>
2023-12-12 23:31:03 +0100

    Add unit tests & test configuration file

---
+++ hooks/conditions/inconfig/customvalueisfalsy_test.go
@@ -0,0 +1,49 @@
+package inconfig
+
+import (
+	"github.com/captainhook-go/captainhook/configuration"
+	"github.com/captainhook-go/captainhook/test"
+	"testing"
+)
+
+func TestCustomValueIsFalsy(t *testing.T) {
+	inOut := test.CreateFakeIO()
+	conf, _ := configuration.NewFactory().CreateConfig("../../../test/files/config/valid_with_custom.json", &configuration.JsonAppSettings{})
+	repo := test.CreateFakeRepo()
+
+	options := configuration.NewOptions(map[string]interface{}{"value": "fiz"})
+	condition := configuration.NewCondition("CaptainHook::InConfig.CustomValueIsFalsy", options)
+
+	action := NewCustomValueIsFalsy(inOut, conf, repo)
+	if !action.IsTrue(condition) {
+		t.Errorf("Custom value should be falsy")
+	}
+}
+
+func TestCustomValueIsNotFalsy(t *testing.T) {
+	inOut := test.CreateFakeIO()
+	conf, _ := configuration.NewFactory().CreateConfig("../../../test/files/config/valid_with_custom.json", &configuration.JsonAppSettings{})
+	repo := test.CreateFakeRepo()
+
+	options := configuration.NewOptions(map[string]interface{}{"value": "foo"})
+	condition := configuration.NewCondition("CaptainHook::InConfig.CustomValueIsFalsy", options)
+
+	action := NewCustomValueIsFalsy(inOut, conf, repo)
+	if action.IsTrue(condition) {
+		t.Errorf("Custom value should not be falsy")
+	}
+}
+
+func TestApplicableFor(t *testing.T) {
+	inOut := test.CreateFakeIO()
+	conf := test.CreateFakeConfig()
+	repo := test.CreateFakeRepo()
+
+	action := NewCustomValueIsFalsy(inOut, conf, repo)
+	if !action.IsApplicableFor("pre-commit") {
+		t.Errorf("Condition should be applicable for 'pre-commit'")
+	}
+	if !action.IsApplicableFor("pre-push") {
+		t.Errorf("Condition should be applicable for 'pre-push'")
+	}
+}
