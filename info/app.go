6ecb9b68d8d0975556e1d9f51aaa4dcdd7d4ddeb
Sebastian Feldmann <sebastianfeldmann@users.noreply.github.com>
2024-12-20 17:04:19 +0100

    Prepare version 1.2.0

--- info/app.go
+++ info/app.go
@@ -1,8 +1,8 @@
 package info
 
 const (
-	Version              = "1.1.0"
-	ReleaseDate          = "2024-11-24"
+	Version              = "1.2.0"
+	ReleaseDate          = "2024-12-20"
 	Config               = "captainhook.json"
 	MinRequiredInstaller = "1.0.4"
 	Executable           = "captainhook"
